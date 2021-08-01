3bb114ea9e3850a970a3d6a2073c91ae03fd76dc
Chris Maracujá <74564883+Maracujacake@users.noreply.github.com>
2021-08-01 12:54:43 -0300

    javas

--- java.js
+++ java.js
@@ -2,9 +2,23 @@ let addAoClicarBotao = document.getElementById('addToDo');
 let ContainerToDo = document.getElementById('toDoContainer');
 let CampoNecessario = document.getElementById('inputField');
 
+
 addAoClicarBotao.addEventListener('click', function(){
     let paragrafo = document.createElement('p')
     paragrafo.innerText = CampoNecessario.value;
     ContainerToDo.appendChild(paragrafo);
     CampoNecessario.value = ""
+    CampoNecessario.style.border = '1px solid red'
+    paragrafo.addEventListener('click', function(){
+    paragrafo.style.textDecoration = 'line-through'
+    paragrafo.style.color = 'red'
+    })
+
+    paragrafo.addEventListener('dblclick', function(){
+        ContainerToDo.removeChild(paragrafo);
+    })
+
+    paragrafo.style.cursor = 'pointer'
 })
+
+  
